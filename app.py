0e2560dde476031e601930e03921772fc63c2d2e
TowhidAhmedd <towhid4635.com>
2026-05-09 17:06:25 +0600

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
+import streamlit as st # type: ignore
 import time
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 from utils.audio_processor import process_input
 from core.transcriber import transcribe_all
 from core.summarizer import summarize, generate_title
